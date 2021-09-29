870c44df4dad8e5da461283a8edc5a7f5b36eea4
Paula Sirisumpund <paulaclark30@yahoo.com>
2021-09-28 23:53:18 -0500

    Added main.js body to grab and run commands

--- main.js
+++ main.js
@@ -1,8 +1,19 @@
-const { Client, Intents } = require('discord.js');
+const { Client, Intents, DiscordAPIError } = require('discord.js'); //Connects to Client
 const client = new Client({intents:[Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]});
+const prefix = '?'; //Prefix the bot will use to execute commands
 
-client.once('ready', () => {
-    console.log('Bot Online');
+client.once('ready', () => { //Checks if bot came online when run
+    console.log('Log Hunter is ready to quest!');
 });
 
-client.login('ODkyNjE5MDMwMDg5ODU5MDk0.YVPiQA.mDGSk_Qfoa8TEJYBQt3CwKof-7Y'); //last line of file
+const fs = require('fs'); 
+client.commands = new Discord.Collection();
+const commandFiles = fs.readdirSync('./commands/').filter(file=> file.endsWith('.js')); //Grabs the .js file that contains code for commands
+
+for(const file of commandFiles){ //loops through the command file to find the correct command
+    const command = require(`./commands/${file}`);
+
+    client.commands.set(command.name, command);
+}
+
+client.login('ODkyNjE5MDMwMDg5ODU5MDk0.YVPiQA.mDGSk_Qfoa8TEJYBQt3CwKof-7Y'); //Make sure this is the last line of this file!
